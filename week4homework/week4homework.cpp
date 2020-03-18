f424630316fab5e0a633a0cece745006fbb7055c
zhangzhuhan <532314058@qq.com>
2020-03-18 20:37:50 +0800

    作业

---
+++ week4homework/week4homework.cpp
@@ -0,0 +1,54 @@
+﻿#include  <iostream>
+#include<iomanip>
+#include  <cmath>
+using  namespace  std;
+
+const  double  PI = 3.1415926;
+//请在此处完善类的设计
+class cylinder
+{
+public:
+	double getvolumn();
+	double getarea();
+	cylinder ( double x,double y );
+	~cylinder();
+private:
+	double a, b;
+};
+
+double cylinder::getvolumn()
+{
+	return (PI * b * b * a);
+}
+double cylinder::getarea()
+{
+	return (2 * PI * b * b + 2 * PI * b * a);
+}
+cylinder::cylinder(double x, double y)
+{
+	a = x;
+	b = y;
+	cout << "构造函数被调用" << endl;
+}
+cylinder::~cylinder()
+{
+	cout << "析构函数被调用" << endl;
+}
+
+int main()
+
+{
+
+	double d, h;
+
+	cin >> d >> h;
+
+	cylinder can(h, d / 2);
+
+	cout << fixed << setprecision(6);
+
+	cout << "油桶的容积是" << can.getvolumn() << " " << endl;
+
+	cout << "铁皮的面积是" << can.getarea() << endl;
+
+}
